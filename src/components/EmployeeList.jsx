eb12533e4d57cafef831dfdc008760a5d99b73a0
Deepak Goyal <deepak84687@gmail.com>
2024-08-03 13:57:33 +0530

    important bug fix

--- src/components/EmployeeList.jsx
+++ src/components/EmployeeList.jsx
@@ -1,17 +1,31 @@
 import React from "react";
 import EmployeeCard from "./EmployeeCard";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const EmployeeList = ({ employees, onDeleteEmployee }) => {
   return (
     <div className="flex flex-col w-[75%] mx-auto mt-[5%]">
       {employees.length === 0 ? (
-        <p className="text-center">No Employees in the system</p>
+        <>
+          <div className="flex flex-col gap-2 justify-between items-center p-2 sm:flex-row ">
+            <h1 className="text-2xl font-bold ">Employees</h1>
+            <div className="bg-green-500 text-white px-2 py-1 rounded">
+              <Link to={`/add`} className="">
+                + Add new employee
+              </Link>
+            </div>
+          </div>
+          <p className="text-center">No Employees in the system</p>
+        </>
       ) : (
         <>
           <div className="flex flex-col gap-2 justify-between items-center p-2 sm:flex-row ">
             <h1 className="text-2xl font-bold ">Employees</h1>
-            <div className="bg-green-500 text-white px-2 py-1 rounded"><Link to={`/add`} className="">+ Add new employee</Link></div>
+            <div className="bg-green-500 text-white px-2 py-1 rounded">
+              <Link to={`/add`} className="">
+                + Add new employee
+              </Link>
+            </div>
           </div>
           {employees.map((employee) => (
             <EmployeeCard
